cf5ca560238b299c8bd521bb52ce4527641c109d
MonikaK28 <monikak6728@gmail.com>
2026-08-19 15:44:30 +0530

    Time: 4 ms (100%), Space: 45.7 MB (87.6%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0)return false;
+        int a=x;
+        int r=0;
+        while(x!=0){
+            int digit=x%10;
+            r=r*10+digit;
+            x=x/10;
+        }
+        return a==r;
+
+    }
+}
