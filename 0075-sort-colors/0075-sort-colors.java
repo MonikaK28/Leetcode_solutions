1f69a9298ed6e2c36200574a05bb29449cc8aaeb
MonikaK28 <monikak6728@gmail.com>
2026-08-28 09:53:34 +0530

    Time: 0 ms (100%), Space: 43.7 MB (23.52%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void sortColors(int[] arr) {
+        int low=0,high=arr.length-1,mid=0;
+        // this is Dutch National Flag Algorithm
+        while(mid<=high){
+            if(arr[mid]==0){
+                int temp=arr[low];
+                arr[low]=arr[mid];
+                arr[mid]=temp;
+                low++;
+                mid++;
+            }
+            else if(arr[mid]==1){
+                mid++;
+            }
+            else{
+                int temp=arr[mid];
+                arr[mid]=arr[high];
+                arr[high]=temp;
+                high--;
+            }
+        }
+    }
+}
