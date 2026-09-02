e8b89bf71871221eccb2f47e1b8292a63625dbe4
MonikaK28 <monikak6728@gmail.com>
2026-09-02 11:42:50 +0530

    Time: 0 ms (100%), Space: 46.8 MB (43.1%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,23 +1,22 @@
 class Solution {
+    int oldcolor,newcolor;
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int oldcolor,newcolor;
         oldcolor=image[sr][sc];
         newcolor=color;
         if(oldcolor==newcolor)return image;
-        dfs(image,sr,sc,oldcolor,newcolor);
+        dfs(image,sr,sc);
         return image;
     }
-        private void dfs(int image[][],int i,int j,int oldcolor,int newcolor){
-            int n=image.length;
-            int m=image[0].length;
-        if(i<0||j<0||i>=n||j>=m)return;
-            if(image[i][j]!=oldcolor){
-            return;
-        }
-        image[i][j]=newcolor;
-        dfs(image,i+1,j,oldcolor,newcolor);//down in row
-        dfs(image,i-1,j,oldcolor,newcolor);//above
-        dfs(image,i,j+1,oldcolor,newcolor);//right in column
-        dfs(image,i,j-1,oldcolor,newcolor);// left in column
+        private void dfs(int image[][],int i,int j){
+            int n=image.length;// for row
+            int m=image[0].length;// for column
+            if(i<0||j<0||i>=n||j>=m||image[i][j]!=oldcolor)return;
+
+            image[i][j]=newcolor;
+            // dfs traversal
+            dfs(image,i+1,j);//down in row
+            dfs(image,i-1,j);//above
+            dfs(image,i,j+1);//right in column
+            dfs(image,i,j-1);// left in column
     }
 }
